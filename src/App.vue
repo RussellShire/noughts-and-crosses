7a68dde18885f3a77441fce2d18d639c763fff12
Russell Shire <russell.shire@itstechnologygroup.com>
2023-05-26 16:32:20 +0100

    Add very basic building blocks of draggable

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
-<script setup>
-// 
+<script>
+    import Draggable from "vue3-draggable";
+
+    export default {
+        components: {
+            Draggable,
+        },
+        data() {
+            return {
+                items: [
+                    {
+                        "id": 1,
+                        "title": One
+                    },
+                    {
+                        "id": 2,
+                        "title": Two
+                    },
+                    {
+                        "id": 3,
+                        "title": Three
+                    },
+                ]
+            }
+            
+        }
+    };
 </script>
 
 <template>
-<h1>hello world</h1>
+    <draggable v-model="items">
+        <template v-slot:item="{item}">
+            <!-- example -->
+            <div>
+                {{item.title}}
+            </div>
+            <!-- or your own template -->
+        </template>
+    </draggable>
+<div></div>
 </template>
 
 <style scoped>
